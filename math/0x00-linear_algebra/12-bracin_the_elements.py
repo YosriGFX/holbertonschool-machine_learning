f09c39d8a5ec83370fd47e1dc3a7db988b6e8735
YosriGFX <YosriG1997@gmail.com>
2020-12-03 04:50:07 +0100

    12-bracin_the_elements.py

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Bracing the Elements'''
+
+
+def np_elementwise(mat1, mat2):
+    '''np elementwise'''
+    return mat1 + mat2,\
+        mat1 - mat2,\
+        mat1 * mat2,\
+        mat1 / mat2
