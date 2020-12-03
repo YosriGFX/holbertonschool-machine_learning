ae82c2228e9ba3f065753c729557c4b0b1cb6206
YosriGFX <YosriG1997@gmail.com>
2020-12-03 05:22:28 +0100

    102-squashed_like_sardines.py

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -6,6 +6,8 @@ import numpy as np
 def cat_matrices(mat1, mat2, axis=0):
     '''cat matrices'''
     try:
-        return np.concatenate((mat1, mat2), axis).tolist()
+        return np.concatenate(
+            (mat1, mat2), axis, out=None
+        ).tolist()
     except ValueError:
         return None
