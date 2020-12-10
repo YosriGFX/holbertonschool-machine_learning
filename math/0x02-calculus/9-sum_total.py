adacba1b84ad09e2556dc912d74af12b6ddc0cd0
YosriGFX <YosriG1997@gmail.com>
2020-12-10 05:58:33 +0100

    9-sum_total

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     '''summation_i_squared'''
-    if (type(n) != int) or (n < 1):
+    if (type(n) != int) or (n <= 0):
         return None
     return sum(map(lambda a: a ** 2, range(n + 1)))
