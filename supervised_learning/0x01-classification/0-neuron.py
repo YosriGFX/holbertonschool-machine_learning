84f67bfdf32ff70881a916efc16f642d9b4f8e22
YosriGFX <YosriG1997@gmail.com>
2021-03-06 00:03:42 +0100

    tired

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -15,6 +15,6 @@ class Neuron:
             raise ValueError('nx must be a positive integer')
         else:
             self.nx = nx
-            self.W = np.random.normal(size(1, self.nx))
+            self.W = np.random.normal(size=(1, self.nx))
             self.b = 0
             self.A = 0
