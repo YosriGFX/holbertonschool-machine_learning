f29713065f777cdd3f9173421923fe4c9f5446fc
YosriGFX <YosriG1997@gmail.com>
2021-03-06 21:51:46 +0100

    poisson.py

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -16,7 +16,7 @@ class Poisson():
         else:
             if type(data) is not list:
                 raise TypeError('data must be a list')
-            elif len(data) < 3:
+            elif len(data) <= 2:
                 raise ValueError('data must contain multiple values')
             else:
                 self.lambtha = sum(data)/len(data)
