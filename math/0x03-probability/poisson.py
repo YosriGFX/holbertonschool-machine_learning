af1aafd0ef74976000854b9733c17b2b7ec174bc
YosriGFX <YosriG1997@gmail.com>
2021-03-06 21:56:04 +0100

    poisson.py

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -21,3 +21,21 @@ class Poisson():
             else:
                 self.lambtha = sum(data)/len(data)
 
+    def pmf(self, k):
+        '''Calculates the value of the PMF
+        for a given number of “successes”'''
+        try:
+            k = int(k)
+            if k < 0:
+                return 0
+            else:
+                fact = 1
+                for i in range(1, k + 1):
+                    fact = fact * i
+                return (
+                    (
+                        2.7182818285**-self.lambtha*self.lambtha**k
+                    ) / fact
+                )
+        except Exception:
+            return 0
