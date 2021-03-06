2b95379b2699568b02df5faf4528bc807c9b34c0
YosriGFX <YosriG1997@gmail.com>
2021-03-06 23:27:01 +0100

    4-calculate_loss.py

--- supervised_learning/0x02-tensorflow/4-calculate_loss.py
+++ supervised_learning/0x02-tensorflow/4-calculate_loss.py
@@ -5,7 +5,8 @@ import tensorflow as tf
 
 def calculate_loss(y, y_pred):
     '''Function that calculates
-    the accuracy of a prediction'''
+    the softmax cross-entropy
+    loss of a prediction'''
     return tf.losses.softmax_cross_entropy(
         y, y_pred
     )
