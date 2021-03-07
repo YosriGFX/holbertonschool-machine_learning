0b226a3d72e6e74957cc876e82aaf4eb4da1c7c2
YosriGFX <YosriG1997@gmail.com>
2021-03-07 19:06:36 +0100

    10-Adam.py

---
+++ supervised_learning/0x03-optimization/10-Adam.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''Adam Upgraded'''
+import tensorflow as tf
+
+
+def create_Adam_op(
+    loss, alpha, beta1, beta2, epsilon
+):
+    '''Function that creates the training
+    operation for a neural network in tensorflow
+    using the Adam optimization algorithm'''
+    return tf.train.AdamOptimizer(
+        alpha,
+        beta1,
+        beta2,
+        epsilon=epsilon
+    ).minimize(loss)
