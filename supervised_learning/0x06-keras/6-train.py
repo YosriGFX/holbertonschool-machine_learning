6e6d61698f45061ea7c654ceccbab7d50f0f5cbb
YosriGFX <YosriG1997@gmail.com>
2021-03-07 20:46:23 +0100

    6-train.py

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -1,22 +1,38 @@
 #!/usr/bin/env python3
-""" doc """
+'''Early Stopping'''
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs,
-                validation_data=None, early_stopping=False,
-                patience=0, verbose=True, shuffle=False):
-    """ doc """
+def train_model(
+    network,
+    data,
+    labels,
+    batch_size,
+    epochs,
+    validation_data=None,
+    early_stopping=False,
+    patience=0,
+    verbose=True,
+    shuffle=False
+):
+    '''Function That trains a model
+    using mini-batch gradient descent
+    to also analyze validaiton data'''
     callbacks = None
     if validation_data:
         if early_stopping:
-            callbacks = [K.callbacks.EarlyStopping(monitor="loss",
-                                                   patience=patience,
-                                                   mode="auto")]
-    return network.fit(data, labels,
-                       batch_size=batch_size,
-                       epochs=epochs,
-                       verbose=verbose,
-                       callbacks=callbacks,
-                       validation_data=validation_data,
-                       shuffle=shuffle)
+            callbacks = [K.callbacks.EarlyStopping(
+                monitor="loss",
+                patience=patience,
+                mode="auto"
+            )]
+    return network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        callbacks=callbacks,
+        validation_data=validation_data,
+        shuffle=shuffle
+    )
