d67e5d9be53cf90ae1298f721eb9756f1d7978ee
YosriGFX <YosriG1997@gmail.com>
2021-03-07 19:51:28 +0100

    5-train.py

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''Train'''
+import tensorflow.keras as K
+
+
+def train_model(
+    network,
+    data,
+    labels,
+    batch_size,
+    epochs,
+    validation_data=None,
+    verbose=True,
+    shuffle=False
+):
+    
+    '''Function That trains a model
+    using mini-batch gradient descent
+    to also analyze validaiton data'''
+    return network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        validation_data=validation_data,
+        shuffle=shuffle
+    )
