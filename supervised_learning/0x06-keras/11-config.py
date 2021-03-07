9871c92afca51a97f931fd3b267792c04156d98e
YosriGFX <YosriG1997@gmail.com>
2021-03-07 21:02:07 +0100

    11-config.py

--- supervised_learning/0x06-keras/11-config.py
+++ supervised_learning/0x06-keras/11-config.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
-""" doc """
+'''Save and Load Configuration'''
 import tensorflow.keras as K
 
 
 def save_config(network, filename):
-    """ doc """
+    '''Function that saves a model’s
+    configuration in JSON format'''
     with open(filename, "w+") as f:
-        f.write(network.to_json())
+        f.write(
+            network.to_json()
+        )
 
 
 def load_config(filename):
-    """ doc """
+    '''Function that loads a model
+    with a specific configuration'''
     with open(filename, "r") as f:
-        return(K.models.model_from_json(f.read()))
+        return(
+            K.models.model_from_json(
+                f.read()
+            )
+        )
