1b1f2df2babf3d10b3c2fc03c967c3009f6c7b83
YosriGFX <YosriG1997@gmail.com>
2021-03-07 15:29:00 +0100

    1-l2_reg_gradient_descent.py

---
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+'''Gradient Descent with L2 Regularization'''
+import numpy as np
+
+
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    '''Function that updates the
+    weights and biases of a neural
+    network using gradient descent
+    with L2 regularization:'''
+    weightBias = weights.copy()
+    dzi = np.subtract(
+        cache['A' + str(L)], Y
+    )
+    for t in reversed(range(1, L + 1)):
+        a = cache['A' + str(t)]
+        b = cache['A' + str(t - 1)]
+        b = weightBias['b' + str(t)]
+        if t == L:
+            dzi = np.subtract(
+                cache['A' + str(L)], Y
+            )
+        else:
+            w = weightBias['W' + str(t + 1)]
+            dzi = np.multiply(
+                1 - (a**2),
+                np.matmul(w.T, dzi)
+            )
+        dwi = np.matmul(
+            dzi, b.T
+        ) / Y.shape[1]
+        dbi = np.mean(
+            dzi, axis=1, keepdims=True
+        )
+        weights['b' + str(t)] = weightBias[
+            'b' + str(t)
+        ] - alpha * (dbi)
+        dwi = dwi + lambtha / Y.shape[
+            1
+        ] * weightBias['W' + str(t)]
+        weights['W' + str(t)] = weightBias[
+            'W'+str(t)
+        ] - alpha * (dwi)
