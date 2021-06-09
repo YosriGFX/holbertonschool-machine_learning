a097332161a0673cbf770aadd122cdeec3de0722
YosriGFX <YosriG1997@gmail.com>
2021-06-09 23:02:29 +0100

    6-grads.py

---
+++ unsupervised_learning/0x00-dimensionality_reduction/6-grads.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''6. Gradients'''
+import numpy as np
+Q_affinities = __import__('5-Q_affinities').Q_affinities
+
+
+def grads(Y, P):
+    '''calculates the gradients of Y'''
+    a, n_dim = Y.shape
+    Q, num = Q_affinities(Y)
+    dY = np.zeros(
+        (a, n_dim)
+    )
+    for i in range(a):
+        diff = Y[i, :] - Y
+        dY[i, :] = np.sum(
+            (
+                np.expand_dims(
+                    ((P - Q) * num).T,
+                    axis=2
+                )[i, :] * diff
+            ), 0
+        )
+    return dY, Q
