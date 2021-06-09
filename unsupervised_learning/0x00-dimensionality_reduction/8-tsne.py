d4178c1f0f5acff5c23d991480aefbf9d36ef5cd
YosriGFX <YosriG1997@gmail.com>
2021-06-09 23:12:34 +0100

    8-tsne.py

---
+++ unsupervised_learning/0x00-dimensionality_reduction/8-tsne.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''8. t-SNE'''
+import numpy as np
+pca = __import__('1-pca').pca
+P_affinities = __import__('4-P_affinities').P_affinities
+grads = __import__('6-grads').grads
+cost = __import__('7-cost').cost
+
+
+def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
+    ''' performs a t-SNE transformation'''
+    return X
