f1c11383ea3944e6b00cf0b1d378b6d8dc99a426
YosriGFX <YosriG1997@gmail.com>
2021-06-09 16:13:22 +0100

    1-intersection.py

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -11,34 +11,39 @@ def intersection(x, n, P, Pr):
             if x <= n:
                 if isinstance(P, np.ndarray)\
                   and len(P.shape) == 1 and P.shape[0] >= 1:
-                    if np.any(P > 1) or np.any(P < 0):
-                        raise ValueError(
-                            'All values in P must be in the range [0, 1]'
-                        )
-                    else:
-                        if np.any(Pr > 1) or np.any(Pr < 0):
+                    if isinstance(Pr, np.ndarray) and Pr.shape == P.shape:
+                        if np.any(P > 1) or np.any(P < 0):
                             raise ValueError(
-                                'All values in Pr must be in the range [0, 1]'
+                                'All values in P must be in the range [0, 1]'
                             )
                         else:
-                            if np.isclose(np.sum(Pr), 1):
-                                com = np.math.factorial(
-                                    n
-                                ) / (
-                                    np.math.factorial(
-                                        x
-                                    ) * np.math.factorial(n-x)
+                            if np.any(Pr > 1) or np.any(Pr < 0):
+                                raise ValueError(
+                                    'All values in Pr must be in the range [0, 1]'
                                 )
-                                return (
-                                    com * pow(
-                                        P, x
-                                    ) * pow(
-                                        1 - P,
-                                        n - x
-                                    )
-                                ) * Pr
                             else:
-                                raise ValueError('Pr must sum to 1')
+                                if np.isclose(np.sum(Pr), 1):
+                                    com = np.math.factorial(
+                                        n
+                                    ) / (
+                                        np.math.factorial(
+                                            x
+                                        ) * np.math.factorial(n-x)
+                                    )
+                                    return (
+                                        com * pow(
+                                            P, x
+                                        ) * pow(
+                                            1 - P,
+                                            n - x
+                                        )
+                                    ) * Pr
+                                else:
+                                    raise ValueError('Pr must sum to 1')
+                    else:
+                        raise TypeError(
+                            'Pr must be a numpy.ndarray with the same shape as P'
+                        )
                 else:
                     raise TypeError('P must be a 1D numpy.ndarray')
             else:
