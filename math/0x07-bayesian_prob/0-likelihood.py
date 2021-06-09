70f2d1777240219c91f8e6b4d3aa6c19d1f07c24
YosriGFX <YosriG1997@gmail.com>
2021-06-09 15:59:16 +0100

    0-likelihood.py

---
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+'''0. Likelihood'''
+import numpy as np
+
+
+def likelihood(x, n, P):
+    '''Calculates the likelihood of obtaining this
+    data given various hypothetical probabilities
+    of developing severe side effects'''
+    if isinstance(n, (int, float)) and n > 0:
+        if isinstance(x, (int, float)) and x >= 0:
+            if x <= n:
+                if isinstance(P, np.ndarray)\
+                  and len(P.shape) == 1 and P.shape[0] >= 1:
+                    if np.any(P <= 1) and np.any(P >= 0):
+                        com = np.math.factorial(
+                            n
+                        ) / (
+                            np.math.factorial(x) * np.math.factorial(n-x)
+                        )
+                        return com * pow(
+                            P, x
+                        ) * pow(
+                            1 - P,
+                            n - x
+                        )
+                    else:
+                        raise ValueError(
+                            'All values in P must be in the range [0, 1]'
+                        )
+                else:
+                    raise TypeError('P must be a 1D numpy.ndarray')
+            else:
+                raise ValueError('x cannot be greater than n')
+        else:
+            raise ValueError(
+                'x must be an integer that is greater than or equal to 0'
+            )
+    else:
+        raise ValueError("n must be a positive integer")
