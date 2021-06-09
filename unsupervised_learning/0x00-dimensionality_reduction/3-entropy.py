da8d3e1f4da0dbe5503677a36468c3a7e678be99
YosriGFX <YosriG1997@gmail.com>
2021-06-09 22:35:15 +0100

    3-entropy.py

---
+++ unsupervised_learning/0x00-dimensionality_reduction/3-entropy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''3. Entropy'''
+import numpy as np
+
+
+def HP(Di, beta):
+    '''calculates the Shannon entropy and
+    P affinities relative to a data point'''
+    P = np.exp(
+        -Di * beta
+    )
+    Sum = np.sum(P)
+    Pi = P / Sum
+    return (
+        -np.sum(
+            Pi * np.log2(Pi)
+        ), Pi
+    )
