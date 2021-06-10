4dfd7a778ab3827f9371eb6c5d377c4ad35b7c94
YosriGFX <YosriG1997@gmail.com>
2021-06-10 02:08:28 +0100

    5-backward.py

---
+++ unsupervised_learning/0x02-hmm/5-backward.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+'''5. The Backward Algorithm'''
+import numpy as np
+
+
+def validator(Observation, Emission, Transition, Initial):
+    '''validator'''
+    if not isinstance(Observation, np.ndarray) or len(Observation.shape) != 1:
+        return False
+    if not isinstance(Emission, np.ndarray) or len(Emission.shape) != 2:
+        return False
+    if not isinstance(Transition, np.ndarray) or len(Transition.shape) != 2:
+        return False
+    if not isinstance(Initial, np.ndarray) or len(Initial.shape) != 2:
+        return False
+    if Transition.shape[0] != Emission.shape[
+        0
+    ] or Transition.shape[1] != Emission.shape[0]:
+        return False
+    if Initial.shape[0] != Emission.shape[0] or Initial.shape[1] != 1:
+        return False
+    if not np.sum(Emission, axis=1).all():
+        return False
+    if not np.sum(Transition, axis=1).all() or not np.sum(Initial) == 1:
+        return False
+    return True
+
+
+def backward(Observation, Emission, Transition, Initial):
+    '''performs the backward algorithm for a hidden markov model'''
+    if validator(Observation, Emission, Transition, Initial):
+        N = Emission.shape[0]
+        T = Observation.shape[0]
+        B = np.zeros((N, T))
+        B[:, T - 1] = np.ones(N)
+        for t in range(T - 2, -1, -1):
+            B[:, t] = np.sum(
+                Transition * Emission[:, Observation[
+                    t + 1
+                ]] * B[:, t + 1], axis=1
+            )
+        P = np.sum(Initial[:, 0] * Emission[:, Observation[
+            0
+        ]] * B[:, 0])
+        return P, B
+    else:
+        return None, None
