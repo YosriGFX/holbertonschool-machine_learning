2c662454ce1e746dbe060ed843acb636551d73b0
YosriGFX <YosriG1997@gmail.com>
2021-06-10 13:41:34 +0100

    1-gp.py

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/1-gp.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+'''1. Gaussian Process Prediction'''
+import numpy as np
+
+
+class GaussianProcess:
+    '''represents a noiseless 1D Gaussian process'''
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        '''Class constructor'''
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
+
+    def kernel(self, X1, X2):
+        '''calculates the covariance kernel
+        matrix between two matrices'''
+        return self.sigma_f ** 2 * np.exp(
+            -0.5 / self.l ** 2 * (
+                np.sum(
+                    X1 ** 2, 1
+                ).reshape(
+                    -1, 1
+                ) + np.sum(
+                    X2 ** 2, 1
+                ) - 2 * np.dot(
+                    X1, X2.T
+                )
+            )
+        )
+
+    def predict(self, X_s):
+        '''predicts the mean and standard
+        deviation of points in a Gaussian process'''
+        K = self.K
+        Inv = np.linalg.inv(K)
+        a = self.kernel(self.X, X_s)
+        mu = a.T.dot(Inv).dot(self.Y)
+        mu = mu.reshape(-1)
+        sigma = self.kernel(X_s, X_s) - a.T.dot(Inv).dot(a)
+        sigma = np.diag(sigma)
+        return mu, sigma
