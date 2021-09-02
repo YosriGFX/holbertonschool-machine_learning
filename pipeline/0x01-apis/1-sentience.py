b0c919a2c75ee4cc88a2bc8ddde304e28156d011
YosriGFX <YosriG1997@gmail.com>
2021-09-02 16:30:06 +0100

    1-sentience.py

---
+++ pipeline/0x01-apis/1-sentience.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''1. Where I am?'''
+import requests
+
+
+def sentientPlanets():
+    '''returns the list of names of the
+    home planets of all sentient species.'''
+    sentient = []
+    request = {'next': 'https://swapi-api.hbtn.io/api/species/'}
+    while request['next'] is not None:
+        request = requests.get(request['next']).json()
+        for row in request['results']:
+            if row['designation'] == 'sentient':
+                url = row['homeworld']
+                if url is not None:
+                    planet = requests.get(url).json()['name']
+                    if planet not in sentient:
+                        sentient.append(planet)
+    return sentient
