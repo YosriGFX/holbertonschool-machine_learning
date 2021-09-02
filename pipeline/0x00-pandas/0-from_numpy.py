363a651a8afcdc3db4ccdbc5cd14884cfe1c67bb
YosriGFX <YosriG1997@gmail.com>
2021-09-02 13:59:18 +0100

    0-from_numpy.py

--- pipeline/0x00-pandas/0-from_numpy.py
+++ pipeline/0x00-pandas/0-from_numpy.py
@@ -7,8 +7,9 @@ import string
 def from_numpy(array):
     '''A function that creates a
     pd.DataFrame from a np.ndarray'''
-    c = array.shape[1]
     return pd.DataFrame(
         array,
-        columns=[string.ascii_uppercase[i] for i in range(c)]
+        columns=[
+            string.ascii_uppercase[i] for i in range(array.shape[1])
+        ]
     )
