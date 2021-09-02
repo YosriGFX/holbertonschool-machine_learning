e9f007901d219758595b686c8b2425b9b64645f0
YosriGFX <YosriG1997@gmail.com>
2021-09-02 13:45:59 +0100

    train.py

---
+++ reinforcement_learning/0x03-policy_gradients/train.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+'''2. Implement the training'''
+import numpy as np
+from policy_gradient import policy
+from policy_gradient import policy_gradient
+
+
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
+    '''A function that implements a full training.'''
+    def play_episode(env, weight, episode):
+        '''A function that plays a single episode.'''
+        state = env.reset()[None, :]
+        state_action_reward_grad = []
+        while True:
+            action, grad = policy_gradient(state, weight)
+            state, reward, done, _ = env.step(action)
+            state = state[None, :]
+            state_action_reward_grad.append((state, action, reward, grad))
+            if done:
+                break
+        env.close()
+        return state_action_reward_grad
+
+    weight = np.random.rand(4, 2)
+    episodes = []
+    for episode in range(nb_episodes):
+        sarg = play_episode(env, weight, episode)
+        T = len(sarg) - 1
+        score = 0
+        for t in range(0, T):
+            _, _, reward, grad = sarg[t]
+            score += reward
+
+            G = np.sum([
+                gamma**sarg[k][2] *
+                sarg[k][2] for k in range(t + 1, T + 1)])
+            weight += alpha * G * grad
+        episodes.append(score)
+        print("{}: {}".format(episode, score), end="\r", flush=False)
+    return episodes
