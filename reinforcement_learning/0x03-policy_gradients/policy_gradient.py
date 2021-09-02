b0aa1dc94a5fbb99cf1af212406d6ec46392e0c0
YosriGFX <YosriG1997@gmail.com>
2021-09-02 13:27:50 +0100

    Compute the Monte-Carlo policy gradient

--- reinforcement_learning/0x03-policy_gradients/policy_gradient.py
+++ reinforcement_learning/0x03-policy_gradients/policy_gradient.py
@@ -15,3 +15,20 @@ def policy(matrix, weight):
     '''A function that computes to policy
     with a weight of a matrix'''
     return soft_max(matrix @ weight)
+
+
+def softmax_grad(softmax):
+    '''A function that computes the gradient
+    of a given softmax'''
+    softmax = softmax.reshape(-1, 1)
+    return np.diagflat(softmax) - softmax @ softmax.T
+
+
+def policy_gradient(state, weight):
+    '''A function that computes the Monte-Carlo policy
+    gradient based on a state and a weight matrix.'''
+    Pi = policy(state, weight)
+    action = np.random.choice(len(Pi[0]), p=Pi[0])
+    log = softmax_grad(Pi)[action, :] / Pi[0, action]
+    gradient = state.T @ log[None, :]
+    return action, gradient
