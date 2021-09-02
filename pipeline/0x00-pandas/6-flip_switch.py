7180fac0d1f956235ad9b8e3df0dc76096d8399f
YosriGFX <YosriG1997@gmail.com>
2021-09-02 14:28:53 +0100

    6-flip_switch.py

---
+++ pipeline/0x00-pandas/6-flip_switch.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''6. Flip it and Switch it'''
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+df = df.iloc[::-1].transpose()
+
+print(df.tail(8))
