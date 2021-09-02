5728712dc8fefc35031c16753436b2a7017c6f10
YosriGFX <YosriG1997@gmail.com>
2021-09-02 16:07:26 +0100

    0-passengers.py

--- pipeline/0x01-apis/0-passengers.py
+++ pipeline/0x01-apis/0-passengers.py
@@ -7,17 +7,17 @@ def availableShips(passengerCount):
     '''returns the list of ships that can
     hold a given number of passengers'''
     ships = []
-    url = 'https://swapi-api.hbtn.io/api/starships/'
-    request = requests.get(url).json()
-    result = request['results']
+    result = []
+    request = {'next': 'https://swapi-api.hbtn.io/api/starships/'}
     while request['next'] is not None:
         url = request['next']
         request = requests.get(url).json()
         result += request['results']
     for row in result:
         try:
-            if int(row['passengers']) >= passengerCount:
-                ships.append(row['name'])
+            passenger = int(row['passengers'])
         except ValueError:
-            pass
+            passenger = 0
+        if passenger >= passengerCount:
+            ships.append(row['name'])
     return ships
