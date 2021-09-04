7e4c52459fe836fe86ca8f169a29111199ad0094
YosriGFX <YosriG1997@gmail.com>
2021-09-04 21:02:21 +0100

    18-valid_email.sql

--- pipeline/0x02-databases/18-valid_email.sql
+++ pipeline/0x02-databases/18-valid_email.sql
@@ -1,4 +1,12 @@
 -- 18. Email validation to sent
-DELIMITER $$ CREATE TRIGGER email_check BEFORE UPDATE ON users FOR EACH ROW BEGIN IF STRCMP(OLD.email, NEW.email) <> 0 THEN SET NEW.valid_email = 0;
+DELIMITER $$
+CREATE TRIGGER email_check
+BEFORE UPDATE
+ON users
+FOR EACH ROW
+BEGIN
+IF STRCMP(OLD.email, NEW.email) <> 0 THEN
+SET NEW.valid_email = 0;
 END IF;
-END$$ DELIMITER;
+END$$
+DELIMITER ;
