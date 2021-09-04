1d5ecf564a75918b0b91d10a8d0a77e03e4e5acc
YosriGFX <YosriG1997@gmail.com>
2021-09-04 21:16:59 +0100

    31-insert_school.py

---
+++ pipeline/0x02-databases/31-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''31. Insert a document in Python'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''A function that inserts a new document
+    in a collection based on kwargs'''
+    return mongo_collection.insert_one(kwargs).inserted_id
