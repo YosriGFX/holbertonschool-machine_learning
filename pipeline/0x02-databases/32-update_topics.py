9ea8317bccb6e192df17a46b363e99982e365417
YosriGFX <YosriG1997@gmail.com>
2021-09-04 21:18:49 +0100

    32-update_topics.py

---
+++ pipeline/0x02-databases/32-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''32. Change school topics'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''A function that changes all topics of
+    a school document based on the name'''
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {
+            "topics": topics
+        }}
+    )
