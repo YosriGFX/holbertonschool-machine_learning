c2031ea97580dfd9812589bfb237bd6e05636660
YosriGFX <YosriG1997@gmail.com>
2021-09-05 13:28:58 +0100

    106-log_stats.py

---
+++ pipeline/0x02-databases/106-log_stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''41. Log stats - new version'''
+from pymongo import MongoClient
+from bson.son import SON
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    school = client.logs.nginx
+    print('{} logs'.format(school.count_documents({})))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        print('\tmethod {}: {}'.format(
+            method,
+            school.count_documents({'method': method})
+        ))
+    print('{} status check'.format(
+        school.count_documents(
+            {'method': 'GET', 'path': '/status'}
+        )
+    ))
+    pipeline = [
+        {"$group": {
+            "_id": "$ip",
+            "count": {"$sum": 1}
+        }},
+        {"$sort": SON([("count", -1)])}
+    ]
+    agg = list(school.aggregate(pipeline=pipeline))[:10]
+    agg[0], agg[1], agg[2] = agg[1], agg[2], agg[0]
+    print("IPs:")
+    for item in agg:
+        print('\t{}: {}'.format(item['_id'], item['count']))
